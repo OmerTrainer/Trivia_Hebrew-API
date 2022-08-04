f583fd5ec5adc5f5bdcc36175d0d4b4a784eff12
eiTeyTrainers <eiTeytrainers@gmail.com>
2022-08-04 16:16:17 +0300

    i am ibnsannenmeaf jeaog

---
+++ src/routes/users.route.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import UsersController from '../controllers/users.controller';
+import { CreateUserDto } from '../dtos/users.dto';
+import { Routes } from '../interfaces/routes.interface';
+import validationMiddleware from '../middlewares/validation.middleware';
+
+class UsersRoute implements Routes {
+  public path = '/users';
+  public router = Router();
+  public usersController = new UsersController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    // this.router.get(``, this.usersController.getUsers);
+    // this.router.get(`/:id(\\d+)`, this.usersController.getUserById);
+    this.router.post(`/create`, this.usersController.createUser);
+    this.router.post(`/AllUsers`, this.usersController.getUsers);
+    // this.router.put(`/:id(\\d+)`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
+  }
+}
+
+export default UsersRoute;
